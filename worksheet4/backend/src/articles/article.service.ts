6c6df3022df2cee2e3a16a0b2bc7883e4cc081d7
Tyler Gee <tyeschool@hotmail.com>
2024-08-18 19:47:52 +1200

    build: initial backend setup

---
+++ worksheet4/backend/src/articles/article.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@nestjs/common';
+import { Article } from './article.schema';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreateArticleDto } from './create-article.dto';
+
+@Injectable()
+export class ArticleService {
+  constructor(
+    @InjectModel(Article.name) private articleModel: Model<Article>,
+  ) {}
+
+  test(): string {
+    return 'Article route testing';
+  }
+
+  async findAll(): Promise<Article[]> {
+    return await this.articleModel.find().exec();
+  }
+
+  async findOne(id: string): Promise<Article> {
+    return await this.articleModel.findById(id).exec();
+  }
+
+  async create(createArticleDto: CreateArticleDto): Promise<Article> {
+    return await this.articleModel.create(createArticleDto);
+  }
+}
